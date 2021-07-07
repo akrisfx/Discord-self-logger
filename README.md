13ec99d5bedeae97640821703e545cfec43b3b6d
Akri <vpotach@gmail.com>
2021-07-08 01:39:46 +0300

    added config.json

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ It works on two accounts. main account and bot.
 It logs only deleted messages, along with photos and files.
 ____ 
 ## Manual
+- delete the commented lines in the code
 - to use it, you need to install [node.js](https://nodejs.org) and write in console on directory ``` npm i ```
-- add tokens, id  channel (where do you want to log) and id servers in code(all by comments in code file [index.js](https://github.com/akrisfx/Discord-self-logger/blob/main/index.js))
+- add tokens, id  channel (where do you want to log) and id servers in config.example.json(all by comments in code file [index.js](https://github.com/akrisfx/Discord-self-logger/blob/main/index.js))
 - write in console 
 ```
     node index.js 
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js-selfbot');
+let config
+try{
+    config = require('./config.json')
+} catch {
+    config = require('./config.example.json')
+}
+
 const clientLog = new Discord.Client();
 const clientCheck = new Discord.Client();
-let tokenLog = "token"; //selflog // token self-bot (common bot soon)
-let token = "token"; // me // your token main acount
+const servers = config.servers // your servers that you want to log 
+const logChannel = config.logServerChannel // id channel where do you want to log 
+let tokenLog = config.logToken; //selflog // token bot in 
+let token = config.checkToken; // me // your token main acount
 let prefix = ">";
-let BotVersion = "1.0";
-const servers =["id server 1"," id server 2", "...", "..."]// your servers that you want to log
-const logChannel = "channel ID where is logging" // id channel where do you want to log
+let BotVersion = "1.1";
+tokenLog = config.logToken;             // delete this str___________________________________________
+token = config.checkToken;              // delete this str___________________________________________
+let tarkovServer = config.tarkovServer  // delete this str___________________________________________
 
 
 clientLog.on('ready', () => {
@@ -28,42 +38,44 @@ clientLog.on('message', message => {
     }
 });
 
-clientCheck.on("messageDelete", function(message){
+clientCheck.on("messageDelete", function(message) {
+    let logServerChannel = '859744234886529044' //delete this string___________________________________________
     if (!servers.includes(message.guild.id)) return;
+
     let setTitles = 'Deleted message'
     if(message.edits[1]) { setTitles = 'Deleted message (edited)' }
-
     messageSended = `${message.createdAt}`
     messageSended = messageSended.substring(messageSended.lastIndexOf('G') - 21, messageSended.lastIndexOf('G'))
     
+    if (message.guild.id == tarkovServer) {logServerChannel = '861283825501077504'} // delete this string___________________________________________
     if(message.attachments.first() != undefined){
         let tmpUrl = message.attachments.first().url;
         let delEmbed = new Discord.MessageEmbed()// log message style, change if you want
+            .setAuthor(message.author.username, "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png")
+            .setTitle(setTitles)
+            .setDescription(message.edits)
+            .addField('Server', `${message.guild.name}`, true)
+            .addField('Channel', `#${message.channel.name}`, true)
+            .addField('Author', `${message.author.tag}`, true)
+            .setColor('#0f7')
+            .addField('Time', `${messageSended}`, true)
+            .setImage(tmpUrl)
+            .setTimestamp();
+            clientLog.channels.cache.get(logServerChannel).send(delEmbed); //id channal where do you want to log
+            
+    } else {
+        let delEmbed = new Discord.MessageEmbed() 
         .setAuthor(message.author.username, "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png")
         .setTitle(setTitles)
         .setDescription(message.edits)
         .addField('Server', `${message.guild.name}`, true)
-        .addField('Channel', `${message.channel.name}`, true)
+        .addField('Channel', `#${message.channel.name}`, true)
         .addField('Author', `${message.author.tag}`, true)
-        .addField('Time', `${messageSended}`, true)
-        .setColor('#0f7')
-        .setImage(tmpUrl)
-        .setTimestamp();
-        clientLog.channels.cache.get(logChannel).send(delEmbed); //id channel where do you want to log
-      }
-      else{
-      let delEmbed = new Discord.MessageEmbed() 
-        .setAuthor(message.author.username, "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png")
-        .setTitle(setTitles)
-        .setDescription(message.edits)
-        .addField('Server', `${message.guild.name}`, true)
-        .addField('Channel', `${message.channel.name}`, true)
-        .addField('Author', `${message.author.tag}`, true)
-        .addField('Time', `${messageSended}`, true)
         .setColor('#ff035f')
+        .addField('Time', `${messageSended}`, true)
         .setTimestamp();
-        clientLog.channels.cache.get(logChannel).send(delEmbed); //id channel where do you want to log
-      }       
-});
+        clientLog.channels.cache.get(logServerChannel).send(delEmbed); //id chennal where do you want to log
+    }
+})
 clientCheck.login(token);
 clientLog.login(tokenLog);
